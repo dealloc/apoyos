ea647037c4767bfd36e53b837e57ccc3f41a05de
Wannes Gennar <wannes.gennar@gmail.com>
2020-03-02 17:46:57 +0100

    added default serializer implementation
    Added JsonDomainEventSerializer as the default implementation of IDomainEventSerializer using JSON

---
+++ Servicebus/Apoyos.Servicebus/Abstractions/Serializers/JsonDomainEventSerializer.cs
@@ -0,0 +1,32 @@
+﻿#pragma warning disable CA2000 // Dispose objects before losing scope. Justification: False positive, see https://github.com/dotnet/roslyn-analyzers/issues/3042
+using System.IO;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Apoyos.Servicebus.Contracts;
+
+namespace Apoyos.Servicebus.Abstractions.Serializers
+{
+    /// <summary>
+    /// Handles (de)serializing domain events using <see cref="JsonSerializer"/>
+    /// </summary>
+    public class JsonDomainEventSerializer : IDomainEventSerializer
+    {
+        /// <inheritdoc cref="IDomainEventSerializer.SerializeAsync{TEvent}" />
+        public async Task<byte[]> SerializeAsync<TEvent>(TEvent domainEvent, CancellationToken cancellationToken = default) where TEvent : class, new()
+        {
+            await using var stream = new MemoryStream();
+            await JsonSerializer.SerializeAsync(stream, domainEvent, options: null, cancellationToken).ConfigureAwait(false);
+
+            return stream.ToArray();
+        }
+
+        /// <inheritdoc cref="IDomainEventSerializer.DeserializeAsync{TEvent}" />
+        public async Task<TEvent> DeserializeAsync<TEvent>(byte[] serialized, CancellationToken cancellationToken = default) where TEvent : class, new()
+        {
+            await using var stream = new MemoryStream(serialized);
+            return await JsonSerializer.DeserializeAsync<TEvent>(stream, options: null, cancellationToken)
+                .ConfigureAwait(false);
+        }
+    }
+}
--- Servicebus/Apoyos.Servicebus/Contracts/IDomainEventSerializer.cs
+++ Servicebus/Apoyos.Servicebus/Contracts/IDomainEventSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Apoyos.Servicebus.Contracts
@@ -12,17 +13,19 @@ namespace Apoyos.Servicebus.Contracts
         /// Serialize a domain event asynchronously.
         /// </summary>
         /// <param name="domainEvent">The domain event to serialize.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to allow cancelling the asynchronous operation.</param>
         /// <typeparam name="TEvent">The type of the domain event to serialize.</typeparam>
         /// <returns>The serialized domain event as a byte array.</returns>
-        Task<byte[]> SerializeAsync<TEvent>(TEvent domainEvent) where TEvent : class, new();
+        Task<byte[]> SerializeAsync<TEvent>(TEvent domainEvent, CancellationToken cancellationToken = default) where TEvent : class, new();
 
         /// <summary>
         /// Deserialize a domain event from a byte array.
         /// </summary>
         /// <param name="serialized">The domain event in it's serialized form.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to allow cancelling the asynchronous operation.</param>
         /// <typeparam name="TEvent">The type of domain event to serialize into.</typeparam>
         /// <returns>An instance of <typeparamref name="TEvent"/> deserialized from <paramref name="serialized"/>.</returns>
         /// <exception cref="NotSupportedException">When the format used by <paramref name="serialized"/> is not supported by this serializer.</exception>
-        Task<TEvent> DeserializeAsync<TEvent>(ReadOnlySpan<byte> serialized) where TEvent : class, new();
+        Task<TEvent> DeserializeAsync<TEvent>(byte[] serialized, CancellationToken cancellationToken = default) where TEvent : class, new();
     }
 }
